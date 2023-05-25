297c0f99267a87dfdf705d1e56d75c21697c62a7
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-05-25 13:47:44 -0400

    Updated formatting and added info

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,18 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Blog from "./components/Blog";
-import About from "./components/About";
 import Guestbook from "./components/Guestbook";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="p-2 m-0 mx-4 mt-8 md:mt-20 lg:mt-32">
-                <div className="flex md:flex-row py-4 flex-col">
-                    <Routes>
-                        <Route path="/" element={<Nav />}>
-                            <Route index element={<Home />} />
-                            <Route path="blog" element={<Blog />} />
-                            <Route path="about" element={<About />} />
-                            <Route path="guestbook" element={<Guestbook />} />
-                        </Route>
-                    </Routes>
-                </div>
-            </div>
+            <Routes>
+                <Route path="/" element={<Nav />}>
+                    <Route index element={<Home />} />
+                    <Route path="blog" element={<Blog />} />
+                    <Route path="guestbook" element={<Guestbook />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
-        <main className="flex-auto w-full sm:w-2/3 md:w-3/4 pt-1 px-2 font-serif flex-row">
+        <main className="flex-auto w-full pt-1 px-2 font-serif flex-row">
             <h1 className="text-2xl font-bold" id="home">
-                Abraham Beltran
+                Welcome to the endgame!
             </h1>
             <p className="my-4">
                 This is my personal website which is currently under
@@ -10,9 +10,54 @@ const Home = () => {
                 the left with login added so that posts can have
                 comments/likes/user posts/profiles
             </p>
+            <h1 className="text-xl font-bold" id="home">
+                What is this all about?
+            </h1>
             <p className="my-4">
-                This website will be developed slowly over the course of 2023.
-                More features and styling coming soon!
+                My name is Abraham, some people coming here may also know me as
+                727.
+            </p>
+            <p className="my-3 mb-8">
+                I'm actually currently looking for a software engineering
+                position so if you stumble upon this website and are hiring or
+                this was attached to an application, browse around and make sure
+                to contact me.
+            </p>
+            <p className="my-4">
+                <p className="text-md font-bold">Topics of discussion:</p>
+                <ul className="list-disc mx-4">
+                    <li>Building and fixing my car</li>
+                    <li>Technical tidbits about important software</li>
+                </ul>
+            </p>
+            <p className="my-4">
+                <p className="text-md font-bold">Interests:</p>
+                <ul className="list-disc mx-4">
+                    <li>Building things</li>
+                    <li>Breaking things</li>
+                    <li>Learning about how the universe functions</li>
+                    <li>Improving my engineering skills</li>
+                    <li>Hiking with my cat</li>
+                </ul>
+            </p>
+
+            <p className="my-4 mt-10">
+                This website is mainly going to be for voicing my opinions while
+                sharing different technical thoughts and learnings as I progress
+                as a developer. Although I don't have a lot of experience
+                writing, I do thoroughly enjoy it and have always been
+                interested in starting a blog which will be hosted here!
+            </p>
+            <p className="my-4">
+                Most of what you currently see will undoubtedly change over time
+                and evolve as I evolve as a developer. Due to this, things are
+                constantly changing and some of the website's core
+                functionalities and technologies might also change.
+            </p>
+            <p className="my-4">
+                If for whatever reason you are expecting to link to something
+                here and it is not here, there will be a chat where you can
+                submit issues/thoughts/etc soon enough.
             </p>
             <p className="my-4">
                 {" "}
