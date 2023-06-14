c9bc7d35a15b4a95f640709f1defd34c937d70ee
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-14 19:25:08 -0400

    Changes

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Post
+# Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {'posts' : posts})
+
+def posts(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post.html", {'post': post})
+
+def about(request):
+    return render(request, "blog/about.html")
--- src/content/test.md
+++
@@ -1,6 +0,0 @@
-# Test
-
-Basically what this blog is about is letting my thoughts about technology out as well as letting the information I learn out into the world so that it is easily accessible by me and other people.
-
-## What would this entail?
-funny you should ask!
